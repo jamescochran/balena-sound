40d3db1dae4fd8dee615861f8e56e3250561a42b
Tomás Migone <tomas@balena.io>
2021-05-26 15:13:05 -0300

    Use UUID instead of hostname for plugin broadcast names
    Closes: #449
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- plugins/airplay/start.sh
+++ plugins/airplay/start.sh
@@ -7,7 +7,7 @@ fi
 
 # --- ENV VARS ---
 # SOUND_DEVICE_NAME: Set the device broadcast name for AirPlay
-SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound AirPlay $(hostname | cut -c -4)"}
+SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound AirPlay $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
 
 echo "Starting AirPlay plugin..."
 echo "Device name: $SOUND_DEVICE_NAME"
